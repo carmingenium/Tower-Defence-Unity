7a524e4ef86ae2c0460209395bcb68034e225f5c
Dağhan Sever <daghansever@gmail.com>
2023-09-05 11:08:43 +0300

    Started Implementing Wave System

--- EnemySpawner.cs
+++ EnemySpawner.cs
@@ -7,16 +7,29 @@ public class EnemySpawner : MonoBehaviour
 {
 
     public int r;
-    public GameObject enemyPrefab;
+    public GameObject[] enemyPrefab;
+    // Tier 1 Enemies: 0 - 4
+    // Tier 2 Enemies: 5 - 8
+    // Tier 3 Enemies: 9 - 11
+    public List<GameObject> chosenEnemies;
+    public int Wave;
+    // ------------------------------------
+
     public GameObject loseScreen;
+    
+
     void Start()
     {
-        for (int i = 0; i < 25; i++)
-        {
-            Vector2 EnemySpawnPoint = CircleSpawnFunction();
-            Vector3 convert = new Vector3(EnemySpawnPoint.x, EnemySpawnPoint.y, 0);
-            Instantiate(enemyPrefab, convert, Quaternion.identity);
-        }
+        WaveSpawner();
+    }
+    private void Update()
+    {
+        // check file, if there is a change, register it
+        // if(change){
+        // Tier 1-2-3 which
+        // Random int = 0-4 or 5-8 or 9-11
+        // chosenEnemies.Add(enemyprefab[int])
+        // }
     }
     public Vector2 CircleSpawnFunction()
     {
@@ -27,5 +40,21 @@ public class EnemySpawner : MonoBehaviour
 
         Vector2 SpawnPoint = new Vector2((float)xlen, (float)ylen);
         return SpawnPoint;
+    }
+    public void WaveSpawner()
+    {
+        // find the right enemies from the wave value
+        bool wave_continue= true;
+        // from file, get values (which enemies to spawn, how many will be spawned)
+        while(wave_continue){
+            for (int i = 0; i < 25; i++) 
+            // from the file, repeat this for loop for every enemy type that will be summoned, for the amount of enemy summon
+            {
+                Vector2 EnemySpawnPoint = CircleSpawnFunction();
+                Vector3 convert = new Vector3(EnemySpawnPoint.x, EnemySpawnPoint.y, 0);
+                Instantiate(chosenEnemies[0], convert, Quaternion.identity);
+            }
+            wave_continue = false;
+        }
     }
 }
